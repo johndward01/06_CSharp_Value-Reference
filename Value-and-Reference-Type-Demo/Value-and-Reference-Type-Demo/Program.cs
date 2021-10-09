1ce1d0fabee589cc1c5dd0565e53875964ce8a95
jdw <johndward01@gmail.com>
2021-10-09 17:00:49 -0500

    update

---
+++ Value-and-Reference-Type-Demo/Value-and-Reference-Type-Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Value_and_Reference_Type_Demo
+{
+    class Program
+    {        
+
+        static void Main(string[] args)
+        {
+            int age = 10;
+            age = 20;
+
+            long myVar1 = long.MinValue;
+            long myVar2 = long.MaxValue;
+
+            ulong myUVar1 = ulong.MinValue;
+            ulong myUVar2 = ulong.MaxValue;
+
+
+            //Console.WriteLine(myUVar1);
+            Console.WriteLine(myUVar2);
+
+
+
+            //// Declaring and Initializing a varibable of type string with the value of "John"
+
+            
+        }
+    }
+}
--- Value_and_Reference_Types_Exercise/ChristmasWithTypes/Program.cs
+++ Value_and_Reference_Types_Exercise/ChristmasWithTypes/Program.cs
@@ -14,7 +14,7 @@ namespace ChristmasWithTypes
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{"PS5","RTX-2080TI", "CyberPunk 2077 Preorder" }; 
+            xmas.Presents =  new string[3]{"PS5","RTX-2080TI", "Battlefield 2042" }; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
